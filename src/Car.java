cd8e1b3ce04a3ac668a9ee0ab4ed033b6bf207b1
ruslanTash <ruslan--t@yandex.ru>
2023-01-30 23:51:28 +0400

    Задачи выполнены

--- src/Car.java
+++ src/Car.java
@@ -12,36 +12,12 @@ public class Car {
     }
 
     public Car(String brand, String model, double engineVolume, String color, int year, String country) {
-        if (brand == null || brand.isEmpty()) {
-            this.brand = "default";
-        } else {
-            this.brand = brand;
-        }
-        if (model == null || model.isEmpty()) {
-            this.model = "default";
-        } else {
-            this.model = model;
-        }
-        if (engineVolume <= 0) {
-            this.engineVolume = 1.5;
-        } else {
-            this.engineVolume = engineVolume;
-        }
-        if (color == null || color.isEmpty()) {
-            this.color = "White";
-        } else {
-            this.color = color;
-        }
-        if (year <= 0) {
-            this.year = 2000;
-        } else {
-            this.year = year;
-        }
-        if (country == null || country.isEmpty()) {
-            this.country = "default";
-        } else {
-            this.country = country;
-        }
+        this.brand = (brand == null || brand.isEmpty()) ? "default" : brand;
+        this.model = (model == null || model.isEmpty()) ? "default" : model;
+        this.engineVolume = (engineVolume <= 0) ? 1.5 : engineVolume;
+        this.color = (color == null || color.isEmpty()) ? "White" : color;
+        this.year = (year <= 0) ? 2000: year;
+        this.country = (country == null || country.isEmpty()) ? "defalt" : country;
     }
 
     public Car() {
