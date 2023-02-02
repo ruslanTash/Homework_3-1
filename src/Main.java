765c594a9d22a97c933fee65e8987af75d853cd9
ruslanTash <ruslan--t@yandex.ru>
2023-02-03 01:09:25 +0400

    Задачи выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
+import transport.*;
+
 public class Main {
     public static void main(String[] args) {
-        Car myCar1 = new Car();
-        myCar1.brand = "Lada";
-        myCar1.model = "Granta";
-        myCar1.engineVolume = 1.7;
-        myCar1.color = "Yellow";
-        myCar1.year = 2015;
-        myCar1.country = "Russia";
 
-//        Car myCar1 = new Car("Lada", "Granta", 1.7, "Yellow", 2015, "Russia" );
-        Car myCar2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Black", 2020, null);
-        Car myCar3 = new Car("BMW", "Z8", 3.0, "Black", 2021, "Germany");
-        Car myCar4 = new Car("KIA", "Sportage IV", 2.4, "Red", 2018, "South Korea");
-        Car myCar5 = new Car("Hyundai", "Avante", 1.6, "Orange", 2016, "South Korea");
+
+        Car myCar1 = new Car("Lada", "Granta", 1.7, "Жёлтый", 2015, "Россия",
+                null, null, null, 0, false, null);
+        Car myCar2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Чёрный", 2020, "Германия",
+                null, null, null, 0, false, new Car.Key(true, true));
+        Car myCar3 = new Car("BMW", "Z8", 3.0, "Чёрный", 2021, "Германия",
+                null, null, null, 0, false, null);
+        Car myCar4 = new Car("KIA", "Sportage IV", 2.4, "Красный", 2018, "Южная Корея",
+                null, null, null, 0, false, null);
+        Car myCar5 = new Car("Hyundai", "Avante", 1.6, "Оранжевый", 2016, "Южная Корея",
+                null, null, null, 0, false, null);
 
         System.out.println(myCar1.toString());
         System.out.println(myCar2.toString());
@@ -20,8 +21,7 @@ public class Main {
         System.out.println(myCar4.toString());
         System.out.println(myCar5.toString());
 
-        Car myCar6 = new Car("", "", -5, "", -1, "");
-        System.out.println(myCar6.toString());
-
     }
+
+
 }
